28cb02fe1c68450ed879d84f47b0445f5cdfb3dc
HRISHI-26 <hrishiwynd26@gmail.com>
2023-07-17 12:01:31 +0530

    Updated 4_String.py file with more functions

--- 4_string.py
+++ 4_string.py
@@ -14,3 +14,21 @@ print(string.lower())
 print(string.isalnum())
 
 print(string.split())
+
+name = "hi there"
+print(name[0]) #access index value
+
+# Slicing substrings
+print(name[0:4])
+print(name[:5])
+print(name[0:])
+
+print(name.endswith("e"))
+print(name.center(200))
+print(name.find("e"))
+print(name.isalpha())
+print(name.isdigit())
+print(name.replace('t', 'h'))
+
+
+
